116d261d0f033993e38392f577f419bbbcb63569
Max <162749285+Cyber-Limon@users.noreply.github.com>
2024-10-14 21:30:38 +0500

    Update Structures_algorithms_1.cpp

--- Structures_algorithms_1.cpp
+++ Structures_algorithms_1.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <ctime>
 
 
@@ -9,24 +9,21 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 
 	srand(time(NULL));
-	
+
 	int const size_sample = 45;
 	int const size_table = size_sample * 1.5;
 	int sample[size_sample];
 	int table[size_table];
-	int transition = 500;
+	int transition = 50;
 	int steps = 0;
-	
-	for (int i = 0; i < size_table; i++) 
+
+	for (int i = 0; i < size_table; i++)
 		table[i] = 0;
 
 	int i = 0;
 	while (i < size_sample) {
-		int r = rand() * rand() % 1000000;
+		int r =  (rand() % 9 + 1) * 100000 + rand() * rand() % 100000;
 		bool flag = true;
-
-		if (r / 100000 == 0)
-			continue;
 
 		for (int n = 0; n < i; n++) {
 			if (sample[n] == r) {
@@ -50,7 +47,8 @@ int main() {
 			cout << endl;
 		cout << r << "\t";
 
-		int pos = ((r / 100000 + r / 10000 % 10) % 10 * 100 + (r / 1000 % 100 + r / 100 % 1000) % 10 * 10 + (r / 10 % 10000 + r % 10) % 10) % size_table;
+		int pos = ((r / 100000 + r / 10000 % 10) % 10 * 100 + (r / 1000 % 100 + r / 100 % 1000) % 10 * 10 
+															+ (r / 10 % 10000 + r % 10) % 10) % size_table;
 
 		if (table[pos] == 0) {
 			table[pos] = r;
@@ -65,16 +63,16 @@ int main() {
 					break;
 				}
 			}
-			
+
 			if (n == transition) {
 				while (table[(pos + n) % size_table] != 0)
 					n++;
 
 				table[(pos + n) % size_table] = r;
 				steps = steps + n + 1;
-			}			
-		}        
-        }
+			}
+		}
+	}
 
 	cout << "\n\nХеш-таблица:\n";
 
@@ -82,7 +80,7 @@ int main() {
 		if (i != 0 and i % 5 == 0)
 			cout << endl;
 		cout << i << ")\t" << table[i] << "\t";
-	}	
+	}
 
 	cout << "\n\nРассчитанные параметры:";
 	cout << "\n- коэффициент заполнения таблицы = " << 1.0 * size_sample / size_table;
